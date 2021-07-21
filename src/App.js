39519f378b43c9dcd657e7cb1fa306961ef0f5d3
pablotruji <andrestrujillocruz.1989@gmail.com>
2021-07-20 21:49:03 -0500

    Segundo commit en español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> vamos a empezar a cambiar esto
         </p>
         <a
           className="App-link"
