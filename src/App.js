ce06945ee1f6d79b9d8d6bd0be2e265dc4a7ee14
pablotruji <andrestrujillocruz.1989@gmail.com>
2021-07-26 09:19:16 -0500

    Tercer commit prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> vamos a empezar a cambiar esto
+        PÁGINA WEB DE ILEPSA
         </p>
         <a
           className="App-link"
